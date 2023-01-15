fa70e866c560aeb69f2f9f9be54b15fab65beabb
Serhii <110534352+hakuna00723@users.noreply.github.com>
2023-01-15 21:15:37 +0100

    fixed console

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -2,6 +2,7 @@ export function fetchCountries(name) {
   return fetch(
     `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`
   ).then(resp => {
+    console.log(resp);
     if (!resp.ok) {
       throw new Error(resp.status);
     }
